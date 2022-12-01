e4cf41d2dfdf5075a42c9fc42c2e72642bc174cd
kyeshmz <kyeshimizu@gmail.com>
2022-12-01 09:27:45 +0900

    add ne wpackage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
-import 'package:pochi/state/network_state/network_state_controller.dart';
 import 'package:pochi/state/sidebar_controller.dart';
 
 class HomePage extends ConsumerWidget {
@@ -13,46 +12,45 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final sidebarState = ref.watch(sidebarStateProvider);
     return MacosWindow(
-      sidebar: Sidebar(
-        minWidth: 200,
-        builder: (context, scrollController) {
-          return SidebarItems(
-            currentIndex: sidebarState,
-            onChanged: (index) {
-              log(index.toString());
-              ref.read(sidebarStateProvider.notifier).update((state) => index);
-            },
-            items: const [
-              SidebarItem(
-                leading: MacosIcon(CupertinoIcons.home),
-                label: Text('Home'),
-              ),
-              SidebarItem(
-                leading: MacosIcon(CupertinoIcons.search),
-                label: Text('Explore'),
-              ),
-            ],
-          );
-        },
-      ),
-      child: Center(
-        child: PushButton(
-          buttonSize: ButtonSize.large,
-          onPressed: () async {
-            print('button pressed');
-            // wrapMain(() async {
-            //   await run(test);
-            // }, debug: true);
-
-            ref.read(networkStateProvider.notifier).getWifiInfo().then((value) {
-              print(ref.read(networkStateProvider).wifiIP.toString());
-              print(ref.read(networkStateProvider).wifiIPv6.toString());
-              print(ref.read(networkStateProvider).wifiName.toString());
-            });
+        sidebar: Sidebar(
+          minWidth: 200,
+          builder: (context, scrollController) {
+            return SidebarItems(
+              currentIndex: sidebarState,
+              onChanged: (index) {
+                log(index.toString());
+                ref
+                    .read(sidebarStateProvider.notifier)
+                    .update((state) => index);
+              },
+              items: const [
+                SidebarItem(
+                  leading: MacosIcon(CupertinoIcons.home),
+                  label: Text('Home'),
+                ),
+                SidebarItem(
+                  leading: MacosIcon(CupertinoIcons.search),
+                  label: Text('Explore'),
+                ),
+              ],
+            );
           },
-          child: const Text('button'),
         ),
-      ),
-    );
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('macos picture'),
+                Column(
+                  children: const [
+                    Text('Mac OS version 11'),
+                    Text('Memory amount')
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
